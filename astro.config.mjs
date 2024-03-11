f8145045d5631693824bc6f4fbed3b38b585b1e3
marco hernandez <mh94.design@gmail.com>
2024-03-10 20:03:36 -0600

    fixed site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://bodomow.github.io/',
+  site: 'https://bodomow.github.io',
   base: '/portfolio',
 });
